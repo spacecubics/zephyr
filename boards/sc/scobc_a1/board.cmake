8eb5d7f6b991601a5c92ca95137e0c8ee7301170
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2025-06-14 22:32:42 +0900

    boards: scobc_a1: Overwrite openocd command
    Since the NOR flash driver for the `SC-OBC Module A1` has not yet been
    merged into the Zephyr SDK, we distribute our own OpenOCD command via
    `west.yml`.
    As a result, we override the path to the Zephyr SDK’s OpenOCD command.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- boards/sc/scobc_a1/board.cmake
+++ boards/sc/scobc_a1/board.cmake
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(openocd "--use-elf" "--config=${BOARD_DIR}/support/openocd-ftdi.cfg")
+set(OPENOCD "${WEST_TOPDIR}/modules/tools/openocd/bin/openocd" CACHE FILEPATH "" FORCE)
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
