e8d3538775c0b69c8c8cce4423bc11177740aac0
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2025-06-14 22:37:07 +0900

    boards: scobc_a1: Cleanup OpenOCD script
    This commit adds the `flash bank` command to support flashing to the
    NOR flash of the `SC-OBC Module A1` using `west flash`.
    Previously, the default OpenOCD interface was `FT2232D`. However,
    starting with the `SC-OBC Module A1 Development kit`, CMSIS-DAP
    interfaces such as the Raspberry Pi Pico Debug Probe will be the
    standard.
    To accommodate multiple debug interfaces, the OpenOCD scripts have
    been cleaned up.
    
    If you still wish to use the legacy FT2232D or FT232R interface,
    please set the following environment variable before running
    `west flash`:
    
      $ export OPENOCD_INTERFACE=FT2232D
      $ west flash
    
      $ export OPENOCD_INTERFACE=FT232R
      $ west flash
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- boards/sc/scobc_a1/board.cmake
+++ boards/sc/scobc_a1/board.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(openocd "--use-elf" "--config=${BOARD_DIR}/support/openocd-ftdi.cfg")
+board_runner_args(openocd "--config=${BOARD_DIR}/support/scobc-a1.cfg")
 set(OPENOCD "${WEST_TOPDIR}/modules/tools/openocd/bin/openocd" CACHE FILEPATH "" FORCE)
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
