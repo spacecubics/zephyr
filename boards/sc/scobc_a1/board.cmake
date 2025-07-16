842ea5c8b7c32ba64820b1323f3ef88532092ed4
Gaetan Perrot <gaetan.perrot@spacecubics.com>
2025-07-16 19:34:55 +0900

    board: scobc-a1: add automatic power cycle after flashing
    Add `trigger_board_power_cycle` procedure in OpenOCD config to
    request a power cycle via the TRCH power cycle register (0x4F000020)
    after programming.
    
    This ensures the bootloader reloads the newly flashed application
    into RAM.
    
    The procedure ends with an explicit `shutdown` to avoid the
    mandatory `reset run` issued by Zephyr’s OpenOCD runner after the
    `cmd-post-verify` hook. Without this, OpenOCD would attempt to reset
    a disconnected board during reboot, which leads to errors.
    
    The function is hooked as `--cmd-post-verify` in the board’s
    `.board.cmake`, so it runs automatically after `west flash` but not
    during `west debug`.
    
    Signed-off-by: Gaetan Perrot <gaetan.perrot@spacecubics.com>

--- boards/sc/scobc_a1/board.cmake
+++ boards/sc/scobc_a1/board.cmake
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(openocd "--config=${BOARD_DIR}/support/scobc-a1.cfg")
+board_runner_args(openocd --cmd-post-verify "trigger_board_power_cycle")
 set(OPENOCD "${WEST_TOPDIR}/modules/tools/openocd/bin/openocd" CACHE FILEPATH "" FORCE)
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
